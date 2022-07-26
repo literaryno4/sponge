8a1f0868e3353a56d769725098a76160b07ff290
Ubuntu <ubuntu@ip-172-31-41-223.ap-east-1.compute.internal>
2022-07-26 14:07:58 +0000

    finish lab1

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,5 +1,7 @@
 #include "byte_stream.hh"
 
+#include <algorithm>
+
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
 // For Lab 0, please replace with a real implementation that passes the
@@ -12,56 +14,70 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) : _stream(), _size(0), _capacity(capacity), _nwritten(0), _nread(0), _input_ended(false) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capa)
+    : buffer(), capacity(capa), end_write(false), end_read(false), written_bytes(0), read_bytes(0) {}
 
 size_t ByteStream::write(const string &data) {
-    size_t l = min(remaining_capacity(), data.size());
-    for (size_t i = 0; i < l; ++i) {
-        _stream.emplace_back(data[i]);
+    size_t canWrite = capacity - buffer.size();
+    size_t realWrite = min(canWrite, data.length());
+    for (size_t i = 0; i < realWrite; i++) {
+        buffer.push_back(data[i]);
     }
-    _size += l;
-    _nwritten += l;
-    return l;
+    written_bytes += realWrite;
+    return realWrite;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    return string(_stream.begin(), _stream.begin() + min(len, _size));
+    size_t canPeek = min(len, buffer.size());
+    string out = "";
+    for (size_t i = 0; i < canPeek; i++) {
+        out += buffer[i];
+    }
+    return out;
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { 
-    size_t l = min(len, _size);
-    for (size_t i = 0; i < l; ++i) {
-        _stream.pop_front();
+void ByteStream::pop_output(const size_t len) {
+    if (len > buffer.size()) {
+        set_error();
+        return;
     }
-    _size -= l;
-    _nread += l;
+    for (size_t i = 0; i < len; i++) {
+        buffer.pop_front();
+    }
+    read_bytes += len;
 }
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    const auto ret = peek_output(len);
-    pop_output(len);
-    return ret;
+    string out = "";
+    if (len > buffer.size()) {
+        set_error();
+        return out;
+    }
+    for (size_t i = 0; i < len; i++) {
+        out += buffer.front();
+        buffer.pop_front();
+    }
+    read_bytes += len;
+    return out;
 }
 
-void ByteStream::end_input() {
-    _input_ended = true;
-}
+void ByteStream::end_input() { end_write = true; }
 
-bool ByteStream::input_ended() const { return _input_ended; }
+bool ByteStream::input_ended() const { return end_write; }
 
-size_t ByteStream::buffer_size() const { return _size; }
+size_t ByteStream::buffer_size() const { return buffer.size(); }
 
-bool ByteStream::buffer_empty() const { return _size == 0; }
+bool ByteStream::buffer_empty() const { return buffer.empty(); }
 
-bool ByteStream::eof() const { return input_ended() && buffer_empty(); }
+bool ByteStream::eof() const { return buffer.empty() && end_write; }
 
-size_t ByteStream::bytes_written() const { return _nwritten; }
+size_t ByteStream::bytes_written() const { return written_bytes; }
 
-size_t ByteStream::bytes_read() const { return _nread; }
+size_t ByteStream::bytes_read() const { return read_bytes; }
 
-size_t ByteStream::remaining_capacity() const { return _capacity - _size; }
+size_t ByteStream::remaining_capacity() const { return capacity - buffer.size(); }
